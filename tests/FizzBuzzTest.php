586ef4c09eff10ddc563c19aa30546c0a92977fb
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-05 13:36:54 +0000

    correct assertion

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,7 +7,7 @@
     public function testReturnsFizzWhenPassed3() {
       $fizzBuzz = new FizzBuzz;
       $result = $fizzBuzz->calculate(3);
-      $this->assertEquals($result, 3);
+      $this->assertEquals($result, 'fizz');
     }
 
   }
