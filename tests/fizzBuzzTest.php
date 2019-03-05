21e8e8e14cca3cd029eb89e6f996f305cfc2eab0
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-05 13:07:56 +0000

    add failing test for fizz

---
+++ tests/fizzBuzzTest.php
@@ -0,0 +1,14 @@
+<?php
+
+  use PHPUnit\Framework\TestCase;
+
+  final class FizzBuzzTest extends TestCase {
+
+    public function testReturnsFizzWhenPassed3() {
+      $fizzBuzz = new FizzBuzz;
+      $result = fizzBuzz.calculate(3);
+      $this->assertEquals($result, 3);
+    }
+
+  }
+?>
